06d546bd1613adc29c68e84dd59eae65087ba30f
williamopv <44485452+williamopv@users.noreply.github.com>
2021-03-14 18:09:07 -0300

    Update list.php

--- list.php
+++ list.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once "Database/database.sql";
+require_once "Database/project.sql";
 if ($_SESSION['username'] == null) {
     echo "<script>alert('Please login.');</script>";
     header("Refresh:0 , url=index.html");
